ee8d968140f9b29c4ffd7ea24495297c2ff8d60a
FelipeRhoden <53407136+FelipeRhoden@users.noreply.github.com>
2020-01-16 11:57:48 -0300

    Teste

--- src/js/bodyPage.js
+++ src/js/bodyPage.js
@@ -1,9 +1,17 @@
-import { TElement } from "../class/model.js";
+import * as parchment from "../class/model.js";
 
-const body = new TElement(document.body);
+const body = new parchment.TElement(document.body);
 
-const title = new TElement("H1");
+const title = new parchment.TElement("H1");
+
+const botao = new TElement("BUTTON")
 
 body.addElement('title',title);
+body.addElement('botao',botao);
 
-title.element.appendChild(document.createTextNode('.parchament'));
+title.addElement('text',new parchment.TText('Novo Titulo'));
+botao.addElement('text',new parchment.TText('Alterar Titulo'));
+
+botao.addEvent('click', () => {
+    title.text.get = prompt('Novo Titulo');
+});
